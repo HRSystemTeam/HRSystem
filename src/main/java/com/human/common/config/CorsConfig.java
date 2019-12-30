6a3bca06f3bceede98f768abf97610512d164884
CHINAINMYHEART <1139914806@qq.com>
2019-12-30 17:17:18 +0800

    跨域

--- src/main/java/com/human/common/config/CorsConfig.java
+++ src/main/java/com/human/common/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.human.common.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,15 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @date 2019/12/30 16:43
  */
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+class CorsFilter {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(true)
-                .maxAge(3600)
-                .allowedHeaders("*");
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowCredentials(true);
+            }
+        };
     }
 }
