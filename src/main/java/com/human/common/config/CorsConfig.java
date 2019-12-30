72a3365550ef6f058d634b43490f8d393b3bb779
CHINAINMYHEART <1139914806@qq.com>
2019-12-30 16:43:47 +0800

    解决跨域问题

---
+++ src/main/java/com/human/common/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.human.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author linwt
+ * @date 2019/12/30 16:43
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
