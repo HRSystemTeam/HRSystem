be22f688879c38bc56e688e0948b67e33f396264
CHINAINMYHEART <1139914806@qq.com>
2020-01-03 09:21:35 +0800

    /logout 跨域

--- src/main/java/com/human/framework/security/handle/LogoutSuccessHandlerImpl.java
+++ src/main/java/com/human/framework/security/handle/LogoutSuccessHandlerImpl.java
@@ -39,11 +39,21 @@ public class LogoutSuccessHandlerImpl implements LogoutSuccessHandler {
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
             throws IOException, ServletException {
         LoginUser loginUser = tokenService.getLoginUser(request);
+        setCorsResponse(request, response);
         if (StringUtils.isNotNull(loginUser)) {
             String userName = loginUser.getUsername();
             // 记录用户退出日志
             AsyncManager.me().execute(AsyncFactory.recordLogininfor(userName, Constants.LOGOUT, "退出成功"));
         }
         ServletUtils.renderString(response, JSON.toJSONString(AjaxResult.error(HttpStatus.SUCCESS, "退出成功")));
+    }
+
+    private void setCorsResponse(HttpServletRequest request, HttpServletResponse response) {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Cache-Control","no-cache");
     }
 }
